005eea7988fd8fd9f523571ddd588114ea34fd6a
Avnash Kumar <avnashk.ee.21@nitj.ac.in>
2023-12-27 13:04:31 +0530

    made changes

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
 
 export const useLogin = () => {
+  
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(null)
   const { dispatch } = useAuthContext()
